754d6872c9c8fbd18ee868dc6452e6daeea3b47a
jack-xia-dp <144161208+jackx111@users.noreply.github.com>
2025-01-21 18:34:17 -0500

    save

--- howso/visuals/visuals.py
+++ howso/visuals/visuals.py
@@ -32,6 +32,7 @@ def plot_feature_importances(
     *,
     num_features_to_plot: t.Optional[int] = None,
     sort_values: bool = True,
+    feature_residuals: t.Optional[float | int] = None,
     title: t.Optional[str] = "Feature Importances",
     xaxis_title: t.Optional[str] = "Feature",
     yaxis_title: t.Optional[str] = "Importance",
@@ -47,6 +48,8 @@ def plot_feature_importances(
         The number of features to plot importances for. If this is None, all features will be plotted.
     sort_values : bool, default True
         Whether to sort the values before plotting them.
+    feature_residuals : float | int, optional
+        The robust feature residuals for calculating and plotting the feature importance error bars.
     title : str, default "Feature Importances"
         The title for the figure.
     xaxis_title : str, default "Feature"
@@ -71,9 +74,26 @@ def plot_feature_importances(
     else:
         feature_importances = feature_importances.iloc[0]
 
+    if feature_residuals is not None:
+        error_value = feature_residuals / len(feature_importances)
+        # Ensure error doesn't go below 0 for each bar
+        error_bars = [min(val, error_value) for val in feature_importances]
+    else:
+        error_bars = None
+
     fig = make_subplots()
     fig.update_layout(title=dict(text=title), xaxis=dict(title=xaxis_title), yaxis=dict(title=yaxis_title))
-    fig.add_trace(go.Bar(x=feature_importances.index, y=feature_importances))
+    fig.add_trace(
+        go.Bar(
+            x=feature_importances.index,
+            y=feature_importances,
+            error_y=dict(
+                type='data',
+                array=error_bars,
+                visible=True
+            ) if error_bars is not None else None
+        )
+    )
 
     return fig
 
